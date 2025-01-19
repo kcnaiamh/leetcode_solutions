460dc5d3ba69fedc40cf6e51e7a4800327ff9969
Naim <naim@kcnaiamh.com>
2025-01-19 06:47:52 +0600

    Binary search problem solved

--- binary_search.py
+++ binary_search.py
@@ -2,4 +2,4 @@ from bisect import bisect_left
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         i = bisect_left(nums, target)
-        return -1 if nums[i] != target else i
+        return -1 if i >= len(nums) or nums[i] != target else i
